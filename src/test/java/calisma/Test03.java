db0fb6061b63cadfbaa74370a220c486a4454235
esahiinnn <esahin3@icloud.com>
2020-12-02 01:22:59 +0300

    Test03 Class'inda degisiklikler

--- src/test/java/calisma/Test03.java
+++ src/test/java/calisma/Test03.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 public class Test03 {
 
     /*  Test1
@@ -29,10 +32,13 @@ public class Test03 {
     @BeforeClass
     public static void setUp(){
         //Ayarlari yapin
-
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         //1- https://github.com/ adresine gidin
-
+        driver.get("https://github.com");
 
     }
 
@@ -40,39 +46,66 @@ public class Test03 {
     public void salptekinTest1(){
 
         //2- arama kutusunu locate edin
+        WebElement searchBox = driver.findElement(By.name("q"));
 
         //3- arama kutusuna salptekin yazin
+        searchBox.sendKeys("salptekin");
+        searchBox.submit();
 
         //4- users'a tiklayin
+        driver.findElement(By.xpath("(//a[@class='menu-item'])[9]")).click();
 
         //5- SULEYMAN ALPTEKIN isimli kullaniciyi secin
+        driver.findElement(By.xpath("//a[@href='/salptekin']")).click();
 
         //6- aciklamasinin "SDET and Full Stack Automation Tester Instructor" oldugunu onaylayin
+        String beklenenAciklama = "SDET and Full Stack Automation Tester Instructor";
+        WebElement actualAciklama = driver.findElement(By.xpath("//*[@id=\"js-pjax-container\"]/div[2]/div/div[1]/div/div[4]/div"));
+        String actualText = actualAciklama.getText();
+        if(actualText.equals(beklenenAciklama)){
+            System.out.println("Aciklama dogru");
+        }else{
+
+            System.out.println("Aciklama yanlis dogru aciklama\n" + actualText);
+        }
 
         //7- takipci sayisini ekrana yazdirin
-    }
-    @Test
-    public void bulutluozTest2(){
+        WebElement takipciSayisi = driver.findElement(By.xpath("//a[@class='link-gray no-underline no-wrap']"));
+        System.out.println(takipciSayisi.getText() + " kisi takip ediyor");
 
-        //1- arama kutusunu locate edin
 
+        driver.navigate().to("https://github.com/");
         //2- arama kutusuna bulutluoz yazip arayin
+        String arananKelime = "bulutluoz";
+        WebElement s2= driver.findElement(By.xpath("//input[@type='text'][1]"));
 
+        s2.sendKeys(arananKelime);
+
+        s2.submit();
         //3- users'a tiklayin Bulutluoz isimli kullaniciyi secin
-
+        driver.findElement(By.xpath("(//a[@class='menu-item'])[9]")).click();
+        driver.findElement(By.xpath("//img[@alt='@bulutluoz']")).click();
         //4- aciklamasinin "selenium code examples for whom learning java and trying to be automation tester" olup olmadini test edin
+        String beklenenAciklamaBulutluoz = "selenium code examples for whom learning java and trying to be automation tester";
+        WebElement actualAciklamaBulutluoz = driver.findElement(By.xpath("//*[@id=\"js-pjax-container\"]/div[2]/div/div[1]/div/div[4]/div"));
+        if(actualAciklamaBulutluoz.getText().equals(beklenenAciklamaBulutluoz)){
+            System.out.println("Aciklama dogru");
+        }else{
+            System.out.println("Aciklama yanlis");
+        }
+
 
         //5- takipci sayisini yazdirin
+        WebElement takipciSayisi2 = driver.findElement(By.xpath("//a[@href='/bulutluoz?tab=followers']"));
 
+        System.out.println(takipciSayisi2.getText() + " kisi takip ediyor");
 
 
     }
-
-
 
     @AfterClass
     public static void tearDown(){
         //6- driverlari kapatin
-        //driver.close();
+        driver.close();
     }
 }
